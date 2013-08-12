e59bc096963409b6e7eb319e8704748298f763b4
Pavel Repin <prepin@gmail.com>
2013-08-12 15:18:06 -0700

    For sure, can't live without these personalizations.

---
+++ personal/paxan.el
@@ -0,0 +1,33 @@
+(prelude-ensure-module-deps
+ '())
+
+
+;; Un-fuck-up Meta and Super keys on OSX and enable fullscreen
+;; toggling via M-Enter.
+(when (and (eq system-type 'darwin) window-system)
+  (prelude-swap-meta-and-super)
+  (global-set-key (kbd "M-RET") 'toggle-frame-fullscreen))
+
+
+(when window-system
+  (set-frame-font "Monaco-15" t)
+  (load-theme 'manoj-dark))
+
+
+;; This tells various "git" commands not to pipe their output through
+;; "less" or similar.
+(setenv "PAGER" "cat")
+
+
+;; At work I sometimes use a frame that's 166 characters wide.  When I
+;; do that, I find that many windows split horizontally -- for
+;; example, if I have a single window, displaying a buffer of source
+;; code, typing C-x v d will split that window so that the new
+;; vc-dir window is to the right.  I hate that.  I want that new
+;; window to be below my source, not next to it.
+;; So this makes that not happen.
+(setq split-width-threshold 500)
+
+
+;; Turn on the auto-revert mode, globally.
+(global-auto-revert-mode t)
