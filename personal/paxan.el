e5c22a14d3f2a78a57412718c1adbfbbbc8cc917
Pavel Repin <prepin@gmail.com>
2013-08-12 16:43:28 -0700

    Define 'after' macro.

--- personal/paxan.el
+++ personal/paxan.el
@@ -2,6 +2,14 @@
  '())
 
 
+;; http://milkbox.net/note/single-file-master-emacs-configuration/
+(defmacro after (mode &rest body)
+  "`eval-after-load' MODE evaluate BODY."
+  (declare (indent defun))
+  `(eval-after-load ,mode
+     '(progn ,@body)))
+
+
 ;; I ain't guru, OK?
 (setq prelude-guru nil)
 
